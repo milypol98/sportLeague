20ac1bb0c7d8d5f054df548066592c53b72b0238
milypol98 <milypol98@gmail.com>
2023-04-13 18:14:56 +0200

    First commit

---
+++ src/main/java/pl/milypol/sportleague/SportLeagueApplication.java
@@ -0,0 +1,13 @@
+package pl.milypol.sportleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportLeagueApplication.class, args);
+	}
+
+}
